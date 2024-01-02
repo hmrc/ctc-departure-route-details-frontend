b7f35ceebe242e1c0bbe815833709278c7603234
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2024-01-02 12:01:01 +0000

    CTCP-4655: Added missing unit test.

--- test/forms/mappings/MappingsSpec.scala
+++ test/forms/mappings/MappingsSpec.scala
@@ -16,7 +16,7 @@
 
 package forms.mappings
 
-import models.{Enumerable, Radioable, RichString}
+import models.{Enumerable, Radioable}
 import org.scalatest.OptionValues
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
@@ -61,6 +61,11 @@ class MappingsSpec extends AnyFreeSpec with Matchers with OptionValues with Mapp
       result.get mustEqual "foobar"
     }
 
+    "must bind a valid string with trailing whitespace" in {
+      val result = testForm.bind(Map("value" -> "foobar   "))
+      result.get mustEqual "foobar"
+    }
+
     "must not bind an empty string" in {
       val result = testForm.bind(Map("value" -> ""))
       result.errors must contain(FormError("value", "error.required"))
@@ -80,59 +85,6 @@ class MappingsSpec extends AnyFreeSpec with Matchers with OptionValues with Mapp
     "must unbind a valid value" in {
       val result = testForm.fill("foobar")
       result.apply("value").value.value mustEqual "foobar"
-    }
-  }
-
-  "textWithSpacesRemoved" - {
-
-    val testForm: Form[String] =
-      Form(
-        "value" -> adaptedText()(_.removeSpaces())
-      )
-
-    "must bind a valid string" - {
-      "with no whitespace" in {
-        val result = testForm.bind(Map("value" -> "foobar"))
-        result.get mustEqual "foobar"
-      }
-
-      "with whitespace" in {
-        val result = testForm.bind(Map("value" -> " foo \t bar "))
-        result.get mustEqual "foobar"
-      }
-    }
-
-    "must not bind an empty string" in {
-      val result = testForm.bind(Map("value" -> ""))
-      result.errors must contain(FormError("value", "error.required"))
-    }
-
-    "must not bind a string of whitespace only" in {
-      val result = testForm.bind(Map("value" -> " \t"))
-      result.errors must contain(FormError("value", "error.required"))
-    }
-
-    "must not bind an empty map" in {
-      val result = testForm.bind(Map.empty[String, String])
-      result.errors must contain(FormError("value", "error.required"))
-    }
-
-    "must return a custom error message" in {
-      val form   = Form("value" -> text("custom.error"))
-      val result = form.bind(Map("value" -> ""))
-      result.errors must contain(FormError("value", "custom.error"))
-    }
-
-    "must unbind a valid value" - {
-      "with no whitespace" in {
-        val result = testForm.fill("foobar")
-        result.apply("value").value.value mustEqual "foobar"
-      }
-
-      "with whitespace" in {
-        val result = testForm.fill(" foo \t bar ")
-        result.apply("value").value.value mustEqual " foo \t bar "
-      }
     }
   }
 
